a4d239e99c918c52308f0bf825461b7fa82374a9
Sofian Ait-Said <s.ait-said@mito.hu>
2024-12-05 14:20:42 +0100

    Day 5

--- src/hu/sofian/aoc2024/day5/Day05.kt
+++ src/hu/sofian/aoc2024/day5/Day05.kt
@@ -2,6 +2,7 @@ package hu.sofian.aoc2024.day5
 
 import hu.sofian.aoc2024.println
 import hu.sofian.aoc2024.readInput
+import java.util.Collections
 
 
 val rules = mutableListOf<Pair<Long, Long>>()
@@ -21,22 +22,79 @@ fun main() {
         }
     }
 
-
     fun part1(input: List<String>): Long {
         parseInput(input)
-        return 0
+        val correctUpdates = mutableListOf<List<Long>>()
+        updates.forEach { update ->
+            if (update.all { pageNumber ->
+                    val applicableRules = rules.filter { it.first == pageNumber }
+                    applicableRules.all {
+                        update.indexOf(it.second) == -1 || (update.indexOf(pageNumber) < update.indexOf(
+                            it.second
+                        ))
+                    }
+                }) {
+                correctUpdates += update
+            }
+        }
+        val result = correctUpdates.sumOf { it[(it.size - 1) / 2] }
+        return result
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
+        parseInput(input)
+        val inCorrectUpdates = mutableListOf<List<Long>>()
+        updates.forEach { update ->
+            if (!update.all { pageNumber ->
+                    val applicableRules = rules.filter { it.first == pageNumber }
+                    applicableRules.all {
+                        update.indexOf(it.second) == -1 || (update.indexOf(pageNumber) < update.indexOf(
+                            it.second
+                        ))
+                    }
+                }) {
+                inCorrectUpdates += update
+            }
+        }
+        val fixedUpdates = mutableListOf<List<Long>>()
+        inCorrectUpdates.forEach { update ->
+            val fixedUpdate = update.toMutableList()
+            do {
+                fixedUpdate.forEach { pageNumber ->
+                    val applicableRule = rules.filter {
+                        it.first == pageNumber && fixedUpdate.indexOf(it.second) != -1 && (fixedUpdate.indexOf(it.first) > fixedUpdate.indexOf(
+                            it.second
+                        ))
+                    }.maxByOrNull { fixedUpdate.indexOf(it.second) }
+                    if (applicableRule != null) {
+                        Collections.swap(
+                            fixedUpdate, fixedUpdate.indexOf(applicableRule.first), fixedUpdate.indexOf(
+                                applicableRule.second
+                            )
+                        )
+                    }
+                }
+            } while (!fixedUpdate.all { pageNumber ->
+                    val applicableRules = rules.filter { it.first == pageNumber }
+                    applicableRules.all {
+                        fixedUpdate.indexOf(it.second) == -1 || (fixedUpdate.indexOf(it.first) < fixedUpdate.indexOf(
+                            it.second
+                        ))
+                    }
+                })
+            fixedUpdates += fixedUpdate
+        }
 
-        return 0
+        val result = fixedUpdates.sumOf { it[(it.size - 1) / 2] }
+        return result
     }
+
 
     // Test if implementation meets criteria from the description, like:
     check(part1(readInput(("Day05_test"))) == 143L)
 
     // Test if implementation meets criteria from the description, like:
-    check(part2(readInput(("Day05_test2"))) == 9)
+    check(part2(readInput(("Day05_test"))) == 123L)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day05")
